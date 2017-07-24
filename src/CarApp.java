2620c08b30cda615530ffeecf777a2d7680c10cf
RedEis91 <imitationkrabs91@gmail.com>
2017-07-23 20:37:12 -0400

    fixed bug

--- src/CarApp.java
+++ src/CarApp.java
@@ -4,6 +4,6 @@ import java.util.ArrayList;
 public class CarApp {
     public static void main(String[] args){
         ArrayList<Car> stock = new ArrayList<Car>();
-        stock.add(new Car())
+        stock.add(new Car());
     }
 }
